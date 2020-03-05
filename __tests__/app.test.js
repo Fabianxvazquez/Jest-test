a646069b39b7139466bac96e17527a03b03973eb
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-04 18:27:14 -0700

    double array

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,14 @@ import {
   doubleArray,
 } from '../app'
 
-describe ('originalArrayTest', () =>{
+describe ('originalArray', () =>{
    test('original array should return itself', ()=>{
      expect(originalArray([1,2,3,4])).toEqual([1,2,3,4])
+   })
+ })
+
+ describe ('doubleArray', () => {
+   test('should double each number in array', ()=>{
+    expect(doubleArray([1,2,3,4])).toEqual([2,4,6,8])
    })
  })
